44e3bd69afea1de326347cb29ed3703d0266414a
Mark Wood <Mark.Wood@dubaicollege.org>
2020-09-17 08:52:54 +0400

    added validation for age and CS Student

--- record_pset_solution.py
+++ record_pset_solution.py
@@ -3,9 +3,15 @@ class Record:
     def __init__(self,forename,surname,age,gender,cs_student):
         self.__forename = forename
         self.__surname = surname
-        self.__age = age
+        if not isinstance(age, int):
+            raise TypeError("age must be an integer")
+        else:
+            self.__age = age
         self.__gender = gender
-        self.__cs_student = cs_student
+        if cs_student not in ["True","False"]:
+            raise ValueError("CS Student must be 'True' or 'False'")
+        else:
+            self.__cs_student = cs_student
 
     def get_forename(self):
         return self.__forename
